7a7ceed7e4bef92a438e2137cc574395e551a0f5
CNBorn <CNBorn@hotmail.com>
2017-03-21 23:22:49 -0400

    add ga

--- douban-game-explorer-enhanced.js
+++ douban-game-explorer-enhanced.js
@@ -87,4 +87,10 @@ var inline_src = (<><![CDATA[
 ]]></>).toString();
 var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
 eval(c.code);
+
+// Google Analytics
+var script = document.createElement('script');
+script.innerHTML = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-329713-14', 'auto'); ga('send', 'pageview');";
+document.body.appendChild(script);
+
 /* jshint ignore:end */
